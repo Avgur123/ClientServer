19645bfa02e9fef6edd9accfea8d561670c33fb1
Avgur123 <scienide@mail.ru>
2017-12-05 12:22:14 +0300

    Last revision two different procs..

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -2,6 +2,7 @@ package com.company;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
@@ -23,5 +24,28 @@ public class Client {
     public Client (){
 
     }
+    public static void main(String[] args) {
+        // write your code here
+        //Client client = new Client("localhost",1212);
 
+        System.out.print("Main: starting server");
+        Scanner input = new Scanner(System.in);
+
+        String ss = input.nextLine();
+
+     /*   if (s=="s") {
+            Server server = new Server();
+
+        }
+        if (s=="c")   {
+
+            System.out.print("Type port for client:");
+            s = input.nextLine();
+            Client client= new Client("localhost",Integer.valueOf(s));
+
+        }*/
+        /*Server server = new Server();
+        server.wait1();*/
+
+    }
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,36 +1,54 @@
 package com.company;
 
-import java.util.Scanner;
+import com.company.Client;
+
 import java.net.ServerSocket;
+import java.util.Scanner;
 
 public class Server {
+
     ServerSocket server;
+
     public Server() {
-//create the Scanner
-        Scanner input = new Scanner(System.in);
-        System.out.print("Type port for server:");
-        String s = input.nextLine();
-        try {
-         server = new ServerSocket(Integer.valueOf(s));
-        } catch (Exception e) {
 
-            System.out.print("Server creation error " + e);
+
+            Scanner input = new Scanner(System.in);
+            System.out.print("Type port for server:");
+            String s = input.nextLine();
+            try {
+                server = new ServerSocket(Integer.valueOf(s));
+            } catch (Exception e) {
+
+                System.out.print("Server creation error " + e);
+
+            }
+        }
+
+        public static void main(String[] args) {
+            // write your code here
+            //Client client = new Client("localhost",1212);
+
+        System.out.print("Creating server...");
+            Server server = new Server();
+            server.wait1();
 
         }
-    }
-    public void wait1 ()
-    {
-        System.out.print("Waiting for a client...");
-        try {
-            server.accept();
-            System.out.println("Client connected");
-        }
-            catch (Exception e) {
+
+        /*Server server = new Server();
+        server.wait1();*/
+
+
+
+        public void wait1 () {
+            System.out.print("Waiting for a client...");
+            try {
+                server.accept();
+                System.out.println("Client connected");
+            } catch (Exception e) {
                 System.out.println("Can't accept");
                 System.exit(-1);
 
             }
 
 
-    }
-}
+        } }
